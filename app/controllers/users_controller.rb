2f4084a2d7c2bf6fae378802f4c0c5766e7e1cd3
kouhei yamasaki <yamasaki.kouhei02@gmail.com>
2022-10-18 22:49:12 +0900

    pry_rails_add_fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name, :icon, :fuel)
+      params.require(:user).permit(:email, :password, :password_confirmation, :name, :avatar, :avatar_cache, :fuel)
   end
 end
