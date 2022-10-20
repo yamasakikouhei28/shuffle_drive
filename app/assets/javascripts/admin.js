b201462b466428d9ed826ccadb125017a38f3f10
kouhei yamasaki <yamasaki.kouhei02@gmail.com>
2022-10-20 19:54:35 +0900

    screan_add

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,4 +1,4 @@
 //= require jquery3
-//= require jquery_ujs   #rails_ujsにしたら何故かエラーになってしまったのでこっち
-//= require admin-lte/plugins/bootstrap/js/bootstrap.bundle.min  #拡張子は省略できる
+//= require jquery_ujs
+//= require admin-lte/plugins/bootstrap/js/bootstrap.bundle.min
 //= require admin-lte/dist/js/adminlte.min
